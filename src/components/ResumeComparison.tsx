c885661cbce7dfdf1b6ffdb93aa0f919643deb0a
Yogesh Khinchi <yogeshkhinchi2005@gmail.com>
2026-01-16 14:43:26 +0530

    Update API URL to production endpoint

--- src/components/ResumeComparison.tsx
+++ src/components/ResumeComparison.tsx
@@ -3,7 +3,7 @@ import { Upload, FileText, ArrowRight, TrendingUp, TrendingDown, Minus, Trophy, 
 import { useDropzone } from 'react-dropzone';
 import { ResumeAnalysis } from '../types/analysis';
 
-let ApiUrl = 'http://localhost:3011'
+let ApiUrl = 'https://resume-analyser-ch1f.onrender.com'
 
 // --- Types matching your Backend Response ---
 interface BattleResult {
