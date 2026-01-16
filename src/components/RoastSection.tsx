90334e18434dd5f7fddf5937051ada5db3cd34ed
Yogesh Khinchi <yogeshkhinchi2005@gmail.com>
2026-01-16 14:44:48 +0530

    Update API URL in RoastSection component

--- src/components/RoastSection.tsx
+++ src/components/RoastSection.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Flame, AlertTriangle } from 'lucide-react';
 
-let ApiUrl = 'https://resume-analyser-mwlu.onrender.com'
+let ApiUrl = 'https://resume-analyser-ch1f.onrender.com'
 
 interface RoastSectionProps {
   resumeText: string;
