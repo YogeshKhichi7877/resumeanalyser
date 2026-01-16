324c834e2814d912e55c69b04bc9f3eb5589c45e
Yogesh Khinchi <yogeshkhinchi2005@gmail.com>
2026-01-16 14:48:30 +0530

    Update API URL for SkillGapLearner component

--- src/components/SkillGapLearner.tsx
+++ src/components/SkillGapLearner.tsx
@@ -14,7 +14,7 @@ import {
 } from 'lucide-react';
 import toast from 'react-hot-toast';
 
-let ApiUrl = 'https://resume-analyser-mwlu.onrender.com'
+let ApiUrl = 'https://resume-analyser-ch1f.onrender.com'
 
 interface Resource {
   title: string;
